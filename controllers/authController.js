061a9c08b4d7ddec5b710b6bdbb1cb8cc0c61b4f
Daniel Charles <danielcharles_500@hotmail.com>
2026-03-09 15:34:33 +0000

    Implement register controller for user authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,22 @@
-exports.register = async (req, res) => { };
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.create({ username, password });
+
+        res.status(201).json({
+            message: "User created",
+            user: {
+                id: user.id,
+                username: user.username
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating user"
+        });
+    }
+};
 
 exports.login = async (req, res) => { };
