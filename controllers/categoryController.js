5529ceaec79230828641d922e5f3c890faa55e01
Daniel Charles <danielcharles_500@hotmail.com>
2026-03-26 08:12:14 +0000

    Refactor items and categories to REST API with JSON responses and correct routing

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,41 +2,40 @@ const { Category } = require('../models');
 
 async function listCategories(req, res) {
     const categories = await Category.findAll();
-    res.render('categories', { categories });
-}
-
-async function createCategoryPage(req, res) {
-    res.render('createCategory');
+    res.json(categories);
 }
 
 async function createCategory(req, res) {
     const { name, description } = req.body;
-    await Category.create({ name, description });
-    res.redirect('/categories');
+
+    const category = await Category.create({ name, description });
+
+    res.status(201).json(category);
 }
 
 async function editCategoryPage(req, res) {
     const category = await Category.findByPk(req.params.id);
-    res.render('updateCategory', { category });
+    res.json(category);
 }
 
 async function updateCategory(req, res) {
     const { name, description } = req.body;
+
     await Category.update(
         { name, description },
         { where: { id: req.params.id } }
     );
-    res.redirect('/categories');
+
+    res.json({ message: "Category updated" });
 }
 
 async function deleteCategory(req, res) {
     await Category.destroy({ where: { id: req.params.id } });
-    res.redirect('/categories');
+    res.json({ message: "Category deleted" });
 }
 
 module.exports = {
     listCategories,
-    createCategoryPage,
     createCategory,
     editCategoryPage,
     updateCategory,
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
-router.get('/categories', categoryController.listCategories);
-router.get('/categories/create', categoryController.createCategoryPage);
-router.post('/categories', categoryController.createCategory);
-router.get('/categories/:id/edit', categoryController.editCategoryPage);
-router.post('/categories/:id', categoryController.updateCategory);
-router.post('/categories/:id/delete', categoryController.deleteCategory);
+router.get('/', categoryController.listCategories);
+router.post('/', categoryController.createCategory);
+router.get('/:id/edit', categoryController.editCategoryPage);
+router.post('/:id', categoryController.updateCategory);
+router.post('/:id/delete', categoryController.deleteCategory);
 
 module.exports = router;
