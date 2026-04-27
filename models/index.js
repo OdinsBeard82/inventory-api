fd2d2c7b05c97f5dc26002187313adc8bf8f9d6a
Daniel Charles <danielcharles_500@hotmail.com>
2026-04-27 14:24:57 +0100

    fix config for Render deployment

--- models/index.js
+++ models/index.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const config = require(__dirname + '/../config/config.json')['development'];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 // Load models using CLI pattern
 const User = require('./user')(sequelize, Sequelize.DataTypes);
